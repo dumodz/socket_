fc27849417d0dd9a71b03e8ee61a0d678a173d25
dumodz <ahmedcool123yes@gmail.com>
2014-05-05 10:51:05 -0700

    Create server.php
    'server.php' base written.

---
+++ server.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  socket_
+  Written by dumodz
+  
+  GitHub: https://github.com/dumodz/socket_/
+*/
+
+class server{
+  private $logger;
+  private $server_;
+  private $client_;
+  
+  function __construct(){
+    $this->logger = new logger();
+  }
+  
+  function socketConnect($address, $port){
+    $this->server_ = stream_socket_server('tcp://' . $address . ':' . $port, $errno, $errstr);
+    
+    if($server !== false){
+      $this->logger->consoleLog('Connected to server');
+      
+      $this->socketLoop();
+    }
+    else{
+      $this->logger->closeLog('Could not connect to server');
+    }
+  }
+  
+  function socketLoop(){
+    for(;;){
+      $this->client_ = @stream_socket_accept($this->server_);
+      
+      $this->logger->consoleLog('Now accepting client connections');
+      
+      if($this->client_){
+        $this->socketAccept();
+      }
+    }
+  }
+  
+  function socketAccept(){
+    $this->logger->consoleLog('New client connection accepted');
+    
+    $this->socketClose();
+  }
+  
+  function socketClose(){
+    fclose($this->client_);
+    
+    $this->logger->consoleLog('Client connection closed');
+  }
+}
+?>
