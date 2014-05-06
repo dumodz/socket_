c655219d7c7f9ab12a35b89f0aae85f5564e68bb
dumodz <ahmedcool123yes@gmail.com>
2014-05-06 08:58:33 -0700

    Create packet.php
    'packet.php' base written.

---
+++ packet.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  socket_
+  Written by dumodz
+  
+  GitHub: https://github.com/dumodz/socket_/
+*/
+
+class packet{
+  private $response_;
+  
+  function handlePacket($read_){
+    /*
+      Packet handling
+      Not written yet
+    */
+  }
+}
+?>
