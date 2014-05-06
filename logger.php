e8fbd89bc65fe25c741cb0f793a77928ba303d8b
dumodz <ahmedcool123yes@gmail.com>
2014-05-06 08:53:49 -0700

    Update logger.php
    'logger.php' base written. [Closing timer]

--- logger.php
+++ logger.php
@@ -8,11 +8,14 @@
 
 class logger{
   function consoleLog($log){
-    echo '[socket_LOG] [' . time() . '] ' . $log . chr(10);
+    echo '[socket_CONSOLE_LOG] [' . time() . '] ' . $log . chr(10);
   }
   
   function closeLog($log){
     echo '[socket_CLOSE_LOG] [' . time() . '] ' . $log . chr(10);
+    echo 'Closing in 5 seconds';
+    sleep(5);
+    die();
   }
 }
 ?>
