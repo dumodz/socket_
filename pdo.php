36f0caa1c8d292d56cd184ce8256f0362bab55f9
dumodz <ahmedcool123yes@gmail.com>
2014-05-06 11:40:01 -0700

    Update pdo.php
    'pdo.php' base written. [PDO connection]

--- pdo.php
+++ pdo.php
@@ -7,10 +7,27 @@
 */
 
 class pdo{
+  private $logger;
+  private $database_;
+  private $table_;
+  private $pdo_;
+  
+  function __construct(){
+    $this->logger = new logger();
+  }
+  
   function pdoConnect($address, $username, $password, $database, $table){
-    /*
-      Still being worked on
-    */
+    $this->database_ = $database;
+    $this->table_ = $table;
+    
+    $this->pdo_ = new PDO('mysql:host=' . $address . ';dbname=' . $database, $username, $password);
+    
+    if($this->pdo_ !== false){
+      $this->logger->consoleLog('Connected to MYSQL server');
+    }
+    else{
+      $this->logger->closeLog('Could not connect to MYSQL server [' . $this->pdo_->errorCode() . '] ' . $this->pdo_->errorInfo());
+    }
   }
 }
 ?>
